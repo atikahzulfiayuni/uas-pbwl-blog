4e67e64d4c2c24400d2bdf9f67c1d8093f1a1ee8
atikahzulfiayuni <atikahzulfi369@gmail.com>
2020-07-08 12:15:01 +0700

    Upload Ketiga

---
+++ database/migrations/2020_07_06_061708_create_tb_photos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTbPhotosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tb_photos', function (Blueprint $table) {
+            $table->increments('photo_id');
+            $table->string('photo_date');
+            $table->string('photo_title');
+            $table->string('photo_text');
+            $table->string('photo_upload');
+            $table->unsignedInteger('post_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tb_photos');
+    }
+}
